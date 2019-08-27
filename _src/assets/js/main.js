cd356e5ad51e90e2bc510f0ca7f8400a72e4b0ee
Migara <evamarcod@gmail.com>
2019-08-27 16:37:53 +0200

    Add all basic functionalities

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,16 +5,28 @@ const board = document.querySelector('.board_js');
 const opt4 = document.querySelector('.opt_4Js');
 const opt6 = document.querySelector('.opt_6Js');
 const opt8 = document.querySelector('.opt_8Js');
+const backCardImage = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
 
 function init(){
-  localStorage.setItem('value', 4);
-  if(localStorage.getItem('value')=== '4'){
+  if(localStorage.getItem('value') === ''){
+    opt4.setAttribute('checked', true);
+    localStorage.setItem('value', 4);
+  }
+  else if(localStorage.getItem('value') === null){
+    opt4.setAttribute('checked', true);
+    localStorage.setItem('value', 4);
+  }
+  else if(localStorage.getItem('value') === undefined){
+    opt4.setAttribute('checked', true);
+    localStorage.setItem('value', 4);
+  }
+  else if(localStorage.getItem('value') === '4'){
     opt4.setAttribute('checked', true);
   }
-  else if(localStorage.getItem('value')=== '6'){
+  else if(localStorage.getItem('value') === '6'){
     opt6.setAttribute('checked', true);
   }
-  else if(localStorage.getItem('value')=== '8'){
+  else if(localStorage.getItem('value') === '8'){
     opt8.setAttribute('checked', true);
   }
   board.innerHTML='';
@@ -22,30 +34,42 @@ function init(){
 
 function show(event){
   const guiltyElement = event.currentTarget;
-  const elementUrl = guiltyElement.getAttribute('dataUrl');
-  guiltyElement.style.backgroundImage = `url(${elementUrl})`;
+  const imgArray = guiltyElement.querySelectorAll('.img');
+  for (const item of imgArray){
+    item.classList.toggle('hidden');
+  }
 }
 
-function createCard(url, pair){
+function createCard(url, name, pair){
   const newElement = document.createElement('li');
   newElement.classList.add('card');
   newElement.setAttribute('dataUrl', url);
   newElement.setAttribute('dataPair', pair);
-  const backCardImage = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
-  newElement.style.backgroundImage = `url(${backCardImage})`;
+  const faceImage = document.createElement('img');
+  faceImage.classList.add('img');
+  faceImage.classList.add('hidden');
+  faceImage.src = url;
+  faceImage.alt = name;
+  const tailImage = document.createElement('img');
+  tailImage.classList.add('img');
+  tailImage.src = backCardImage;
+  tailImage.alt = 'default';
+  newElement.appendChild(faceImage);
+  newElement.appendChild(tailImage);
   board.appendChild(newElement);
   newElement.addEventListener('click', show);
 }
 
 function select(){
   const selectedDif = localStorage.getItem('value');
-  fetch(`https://raw.githubusercontent.com/Adalab/cards-data/master/${selectedDif}.json`)
+  const ENDPOINT = `https://raw.githubusercontent.com/Adalab/cards-data/master/${selectedDif}.json`;
+  fetch(ENDPOINT)
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
       for (const item of data){
-        createCard(item.image, item.pair);
+        createCard(item.image, item.name, item.pair);
       }
     });
   button.setAttribute('disabled', true);
