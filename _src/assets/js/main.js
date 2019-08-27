f81a3e5a80dbd576c293db772d4d58a1d72d296b
Migara <evamarcod@gmail.com>
2019-08-28 01:46:13 +0200

    remake functions

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,35 +3,43 @@
 const button = document.querySelector('.btn__js');
 const board = document.querySelector('.board_js');
 const opt4 = document.querySelector('.opt_4Js');
-const opt6 = document.querySelector('.opt_6Js');
-const opt8 = document.querySelector('.opt_8Js');
+const difArray = document.querySelectorAll('.dif__option');
 const backCardImage = 'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB';
 
 function init(){
-  localStorage.removeItem('pair');
-  if(localStorage.getItem('value') === ''){
-    opt4.setAttribute('checked', true);
-    localStorage.setItem('value', 4);
-  }
-  else if(localStorage.getItem('value') === null){
-    opt4.setAttribute('checked', true);
-    localStorage.setItem('value', 4);
-  }
-  else if(localStorage.getItem('value') === undefined){
-    opt4.setAttribute('checked', true);
-    localStorage.setItem('value', 4);
-  }
-  else if(localStorage.getItem('value') === '4'){
-    opt4.setAttribute('checked', true);
-  }
-  else if(localStorage.getItem('value') === '6'){
-    opt6.setAttribute('checked', true);
-  }
-  else if(localStorage.getItem('value') === '8'){
-    opt8.setAttribute('checked', true);
-  }
+  opt4.setAttribute('checked', true);
+  localStorage.setItem('value', 4);
   board.innerHTML='';
+  if(localStorage.getItem('pair') !==null){
+    localStorage.removeItem('pair');
+  }
 }
+function setDifficulty(event){
+  const selectedOpt = event.currentTarget;
+  selectedOpt.setAttribute('checked', true);
+  localStorage.setItem('value', selectedOpt.value);
+}
+function createCard(url, name, pair){
+  const newElement = document.createElement('li');
+  newElement.classList.add('card');
+  newElement.setAttribute('dataUrl', url);
+  newElement.setAttribute('dataPair', pair);
+  newElement.setAttribute('disabled', false);
+  const faceImage = document.createElement('img');
+  faceImage.classList.add('img');
+  faceImage.classList.add('hidden');
+  faceImage.src = url;
+  faceImage.alt = name;
+  const tailImage = document.createElement('img');
+  tailImage.classList.add('img');
+  tailImage.src = backCardImage;
+  tailImage.alt = 'default';
+  newElement.appendChild(faceImage);
+  newElement.appendChild(tailImage);
+  board.appendChild(newElement);
+  newElement.addEventListener('click', showCard);
+}
+
 function comparePair(element, firstPair){
   const secondPair = element.getAttribute('dataPair');
   if(firstPair === secondPair){
@@ -65,41 +73,20 @@ function checkPair(element){
     element.setAttribute('disabled', true);
   }
 }
-function show(event){
-  const guiltyElement = event.currentTarget;
-  const able = guiltyElement.getAttribute('disabled');
-  if(able === 'false'){
-    const imgArray = guiltyElement.querySelectorAll('.img');
+
+function showCard(event){
+  const selectedCard = event.currentTarget;
+  const disabled = selectedCard.getAttribute('disabled');
+  if(disabled === 'false'){
+    const imgArray = selectedCard.querySelectorAll('.img');
     for (const item of imgArray){
       item.classList.toggle('hidden');
     }
-    setTimeout(checkPair(guiltyElement),6000);
   }
+  checkPair(selectedCard);
 }
 
-
-function createCard(url, name, pair){
-  const newElement = document.createElement('li');
-  newElement.classList.add('card');
-  newElement.setAttribute('dataUrl', url);
-  newElement.setAttribute('dataPair', pair);
-  newElement.setAttribute('disabled', false);
-  const faceImage = document.createElement('img');
-  faceImage.classList.add('img');
-  faceImage.classList.add('hidden');
-  faceImage.src = url;
-  faceImage.alt = name;
-  const tailImage = document.createElement('img');
-  tailImage.classList.add('img');
-  tailImage.src = backCardImage;
-  tailImage.alt = 'default';
-  newElement.appendChild(faceImage);
-  newElement.appendChild(tailImage);
-  board.appendChild(newElement);
-  newElement.addEventListener('click', show);
-}
-
-function select(){
+function startGame(){
   const selectedDif = localStorage.getItem('value');
   const ENDPOINT = `https://raw.githubusercontent.com/Adalab/cards-data/master/${selectedDif}.json`;
   fetch(ENDPOINT)
@@ -114,14 +101,9 @@ function select(){
   button.setAttribute('disabled', true);
 }
 
-function setvalue(event){
-  const value = event.currentTarget.value;
-  localStorage.setItem('value', value);
+button.addEventListener('click', startGame);
+for (const item of difArray){
+  item.addEventListener('click', setDifficulty);
 }
-
-button.addEventListener('click', select);
-opt4.addEventListener('click', setvalue);
-opt6.addEventListener('click', setvalue);
-opt8.addEventListener('click', setvalue);
 
 init ();
