497b8f0e8b97139c21fce3691726abd77bf4e8aa
Migara <evamarcod@gmail.com>
2019-08-29 18:02:19 +0200

    make for of work

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -9,20 +9,80 @@ const backCardImage = 'assets/images/pokemon-logo.png';
 let counter = 0;
 let cardsArray = [];
 
-function init(){
+function init() {
   opt4.setAttribute('checked', true);
   localStorage.setItem('value', 4);
-  board.innerHTML='';
-  if(localStorage.getItem('pair') !==null){
+  board.innerHTML = '';
+  if (localStorage.getItem('pair') !== null) {
     localStorage.removeItem('pair');
   }
 }
-function setDifficulty(event){
+function setDifficulty(event) {
   const selectedOpt = event.currentTarget;
   selectedOpt.setAttribute('checked', true);
   localStorage.setItem('value', selectedOpt.value);
 }
-function createCard(url, name, pair){
+
+function checkWin() {
+  const storedValue = parseInt(localStorage.getItem('value'));
+  if (counter === storedValue) {
+    text.innerHTML = '¡Has ganado!';
+    button.removeAttribute('disabled');
+    counter = 0;
+    board.innerHTML = '';
+    cardsArray = [];
+  }
+}
+const checkPair = (element) => {
+  const firstPair = localStorage.getItem('pair');
+  if (firstPair !== null) {
+    const secondPair = element.getAttribute('dataPair');
+    if (firstPair === secondPair) {
+      const pairArray = board.querySelectorAll(`[dataPair = '${firstPair}']`);
+      for (const item of pairArray) {
+        item.classList.add('noVisibility');
+      }
+      localStorage.removeItem('pair');
+      text.innerHTML = '¡Has encontrado una pareja!';
+      counter += 2;
+
+    }
+    else {
+      text.innerHTML = 'No son iguales';
+      element.setAttribute('disabled', true);
+      const disabledLis = board.querySelectorAll(`[disabled = 'true']`);
+      for (const item of disabledLis) {
+        const imgArray = item.querySelectorAll('.img');
+        item.setAttribute('disabled', false);
+        for (const item of imgArray) {
+          item.classList.toggle('hidden');
+        }
+      }
+      localStorage.removeItem('pair');
+    }
+    setTimeout(checkWin, 300);
+  }
+  else {
+    const choosedPair = element.getAttribute('dataPair');
+    localStorage.setItem('pair', choosedPair);
+    element.setAttribute('disabled', true);
+  }
+};
+
+function showCard(event) {
+  text.innerHTML = '';
+  const selectedCard = event.currentTarget;
+  const disabled = selectedCard.getAttribute('disabled');
+  if (disabled === 'false') {
+    selectedCard.setAttribute('disabled', true);
+    const imgArray = selectedCard.querySelectorAll('.img');
+    for (const item of imgArray) {
+      item.classList.toggle('hidden');
+    }
+    setTimeout(checkPair, 1000, selectedCard);
+  }
+}
+function createCard(url, name, pair) {
   const newElement = document.createElement('li');
   newElement.classList.add('card');
   newElement.setAttribute('dataUrl', url);
@@ -39,128 +99,71 @@ function createCard(url, name, pair){
   tailImage.alt = 'default';
   newElement.appendChild(faceImage);
   newElement.appendChild(tailImage);
-  cardsArray.push(newElement);
+  board.appendChild(newElement);
+  newElement.addEventListener('click', showCard);
+  //cardsArray.push(newElement);
 }
-function checkWin(){
-  const storedValue = parseInt(localStorage.getItem('value'));
-  if (counter === storedValue){
-    text.innerHTML = '¡Has ganado!';
-    button.removeAttribute('disabled');
-    counter = 0;
-    board.innerHTML='';
-    cardsArray = [];
-  }
-}
-const checkPair = (element) =>{
-  const firstPair = localStorage.getItem('pair');
-  if(firstPair !== null){
-    const secondPair = element.getAttribute('dataPair');
-    if(firstPair === secondPair){
-      const pairArray = board.querySelectorAll(`[dataPair = '${firstPair}']`);
-      for(const item of pairArray){
-        item.classList.add('noVisibility');
-      }
-      localStorage.removeItem('pair');
-      text.innerHTML = '¡Has encontrado una pareja!';
-      counter += 2;
-
-    }
-    else{
-      text.innerHTML = 'No son iguales';
-      element.setAttribute('disabled', true);
-      const disabledLis = board.querySelectorAll(`[disabled = 'true']`);
-      for(const item of disabledLis){
-        const imgArray = item.querySelectorAll('.img');
-        item.setAttribute('disabled', false);
-        for (const item of imgArray){
-          item.classList.toggle('hidden');
-        }
-      }
-      localStorage.removeItem('pair');
-    }
-    setTimeout (checkWin, 300);
-  }
-  else{
-    const choosedPair = element.getAttribute('dataPair');
-    localStorage.setItem('pair', choosedPair);
-    element.setAttribute('disabled', true);
-  }
-};
-
-function showCard(event){
-  text.innerHTML = '';
-  const selectedCard = event.currentTarget;
-  const disabled = selectedCard.getAttribute('disabled');
-  if(disabled === 'false'){
-    selectedCard.setAttribute('disabled', true);
-    const imgArray = selectedCard.querySelectorAll('.img');
-    for (const item of imgArray){
-      item.classList.toggle('hidden');
-    }
-    setTimeout(checkPair, 1000, selectedCard);
-  }
-}
-
-function startGame(){
+function startGame() {
   const selectedDif = localStorage.getItem('value');
   const ENDPOINT = `https://pokeapi.co/api/v2/pokemon/`;
   fetch(ENDPOINT)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
+    .then(response => response.json())
+    .then(data => {
       const cards = [];
       const selectedCards = [];
-      for( let i = 0; i<= (selectedDif-1)/2; i++){
-        let item = data.results[Math.floor(Math.random()*data.results.length)];
+      for (let i = 0; i <= (selectedDif - 1) / 2; i++) {
+        let item = data.results[Math.floor(Math.random() * data.results.length)];
         cards.push(item);
         const index = data.results.indexOf(item);
-        data.results.splice(index,1);
+        data.results.splice(index, 1);
       }
-      for(const item of cards){
-        fetch(item.url)
-          .then(function(response) {
-            return response.json();
-          })
-          .then(function(data) {
+      const promises = [];
+      const createPromise = (url) =>
+        fetch(url)
+          .then(response => response.json());
+      for (const item of cards) {
+        const promise = createPromise(item.url);
+        promises.push(promise);
+      }
+      Promise.all(promises)
+        .then(data => {
+          for (const item of data){
+            const backImage = item.sprites.back_default;
+            const frontImage = item.sprites.front_default;
+            const name = item.name;
+            const pair = item.id;
             const pokemon = {
-              'image':data.sprites.back_default,
-              'name': data.name,
-              'pair' : data.id
+              'image': backImage,
+              'name': name,
+              'pair': pair
             };
             const pokemon2 = {
-              'image':data.sprites.front_default,
-              'name': data.name,
-              'pair' : data.id
+              'image': frontImage,
+              'name': name,
+              'pair': pair
             };
             selectedCards.push(pokemon);
             selectedCards.push(pokemon2);
-            // Cuándo se pinta esto?
-            console.log('--- AHORA Y AQUÍ TENGO LOS DATOS ---');
-            console.log(selectedCards);
-            console.log('--- SERÍA GUAY, CONSEGUIR TODOS LOS DATOS Y LUEGO PINTAR, SI SOLO TUVIÉSEMOS UNA FORMA DE HACER PETICIONES Y CUANDO TODAS ESTÉN HACER ALGO ---');
-            console.log('--- TAMBIÉN MOLARÍA HACER FUNCIONES FLECHA ---');
-          });
-      }
-      console.log('--- AQUÍ PUEDO O NO PUEDO TENERLOS, SEGURAMENTE NO PORQUE LAS PETICIONES VAN A SU ROLLO ---');
-      console.log('soy selected', selectedCards);
-      console.log('soy la long de selected', selectedCards.length);
 
-      for(const item of selectedCards){
-        console.log('soy un elemento de', selectedCards);
-        const newElement = createCard(item.image, item.name, item.pair);
-        board.appendChild(newElement);
-        newElement.addEventListener('click', showCard);
-      }
-    });
+            for (const item2 of selectedCards){
+              const cardImage = item2.image;
+              const cardName = item2.name;
+              const cardPair = item2.pair;
+              createCard(cardImage, cardName, cardPair);
+            } data.splice(item,1);
 
+          }
+        }
+        );
+    }
+    );
   button.setAttribute('disabled', true);
 }
 
 button.addEventListener('click', startGame);
 
-for (const item of difArray){
+for (const item of difArray) {
   item.addEventListener('click', setDifficulty);
 }
 
-init ();
+init();
